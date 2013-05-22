90e6aeb6469c7e1e68cbc96f7997a7b353d0a455
Andrew Cordery <cordery@gmail.com>
2013-05-22 17:56:26 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='django-countries-plus',
-    version='0.1.0',
+    version='0.1.2',
     author='Andrew Cordery',
     author_email='cordery@gmail.com',
-    packages=['countriesplus',],
+    packages=['countries_plus',],
     url='http://pypi.python.org/pypi/django-countries-plus/',
     license='LICENSE.txt',
     description='A django model & fixture containing all data from the countries table of Geonames.org',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "Django >= 1.2",
     ],
