f829c15604318e935827b4aa1369323589db3896
Andrew Cordery <cordery@gmail.com>
2024-01-11 13:11:31 +0100

    Bump version: 2.1.0 → 2.2.0

--- countries_plus/__init__.py
+++ countries_plus/__init__.py
@@ -1,7 +1,7 @@
 import django
 
 APP_NAME = "countries_plus"
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 if django.VERSION[0] == 2:
     default_app_config = "countries_plus.apps.DefaultConfig"
